cbabbd7a199a56385930328770a1b050a6275f92
Tasniem Seiam <tasniemseiam@gmail.com>
2024-08-27 15:14:10 +0300

    landing movies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-
-import './App.css';
-import DataList from './views/DataList';
-import Footer from './views/footer';
-import MovieList from './views/MovieList';
+import "./App.css";
+// import DataList from './views/DataList';
+import Footer from "./views/footer";
+import MovieList from "./views/MovieList";
 
 function App() {
   return (
     <>
       <MovieList />
-      <Footer/>
+      <Footer />
     </>
   );
 }
